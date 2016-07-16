e36cb7b33c0f230283c78d232250705b587bbf4e
wanek96 <wanek96@mail.ru>
2016-07-16 16:48:22 +0300

    web api start

--- WebUI/Global.asax.cs
+++ WebUI/Global.asax.cs
@@ -3,8 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
+using WebUI.App_Start;
 using WebUI.Binders;
 using WebUI.Infrastructure;
 
@@ -15,6 +17,7 @@ namespace WebUI
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory());
             ModelBinders.Binders.Add(typeof(Cart), new CartModelBinder());
